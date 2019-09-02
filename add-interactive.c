matcheydj/git

built-in add -p: respect the `interactive.singlekey` config setting
The Perl version of `git add -p` supports this config setting to allow
users to input commands via single characters (as opposed to having to
press the <Enter> key afterwards).

This is an opt-in feature because it requires Perl packages
(Term::ReadKey and Term::Cap, where it tries to handle an absence of the
latter package gracefully) to work. Note that at least on Ubuntu, that
Perl package is not installed by default (it needs to be installed via
`sudo apt-get install libterm-readkey-perl`), so this feature is
probably not used a whole lot.

In C, we obviously do not have these packages available, but we just
introduced `read_single_keystroke()` that is similar to what
Term::ReadKey provides, and we use that here.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

add-interactive.c
@@ -69,6 +69,10 @@ int init_add_i_state(struct repository *r, struct add_i_state *state)
 				  &state->interactive_diff_algorithm))
 		state->interactive_diff_algorithm = NULL;
 
+	if (git_config_get_bool("interactive.singlekey",
+				&state->use_single_key))
+		state->use_single_key = 0;
+
 	return 0;
 }
 

vikkyGit/git-1

add-interactive.c
@@ -66,6 +66,10 @@ int init_add_i_state(struct repository *r, struct add_i_state *s)
 				  &s->interactive_diff_algorithm))
 		s->interactive_diff_algorithm = NULL;
 
+	if (git_config_get_bool("interactive.singlekey",
+				&s->use_single_key))
+		s->use_single_key = 0;
+
 	return 0;
 }
 