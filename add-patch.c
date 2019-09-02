matcheydj/git

built-in add -p: offer a helpful error message when hunk navigation failed
... just like the Perl version currently does...

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

add-patch.c
@@ -364,18 +364,36 @@ soft_increment:
 				if (hunk->use == UNDECIDED_HUNK)
 					hunk->use = SKIP_HUNK;
 			}
-		} else if (hunk_index && state->answer.buf[0] == 'K')
-			hunk_index--;
-		else if (hunk_index + 1 < state->hunk_nr &&
-			 state->answer.buf[0] == 'J')
-			hunk_index++;
-		else if (undecided_previous >= 0 &&
-			 state->answer.buf[0] == 'k')
-			hunk_index = undecided_previous;
-		else if (undecided_next >= 0 && state->answer.buf[0] == 'j')
-			hunk_index = undecided_next;
-		else
-			color_fprintf(stdout, state->state.help_color,
+		} else if (state->answer.buf[0] == 'K') {
+			if (hunk_index)
+				hunk_index--;
+			else
+				color_fprintf_ln(stderr,
+						 state->state.error_color,
+						 _("No previous hunk"));
+		} else if (state->answer.buf[0] == 'J') {
+			if (hunk_index + 1 < state->hunk_nr)
+				hunk_index++;
+			else
+				color_fprintf_ln(stderr,
+						 state->state.error_color,
+						 _("No next hunk"));
+		} else if (state->answer.buf[0] == 'k') {
+			if (undecided_previous >= 0)
+				hunk_index = undecided_previous;
+			else
+				color_fprintf_ln(stderr,
+						 state->state.error_color,
+						 _("No previous hunk"));
+		} else if (state->answer.buf[0] == 'j') {
+			if (undecided_next >= 0)
+				hunk_index = undecided_next;
+			else
+				color_fprintf_ln(stderr,
+						 state->state.error_color,
+						 _("No next hunk"));
+		} else
+			color_fprintf(stdout, state->state.error_color,
 				      _(help_patch_text));
 	}
 

vikkyGit/git-1

add-patch.c
@@ -34,6 +34,18 @@ struct add_p_state {
 	size_t hunk_nr, hunk_alloc;
 };
 
+static void err(struct add_p_state *s, const char *fmt, ...)
+{
+	va_list args;
+
+	va_start(args, fmt);
+	fputs(s->s.error_color, stderr);
+	vfprintf(stderr, fmt, args);
+	fputs(s->s.reset_color, stderr);
+	fputc('\n', stderr);
+	va_end(args);
+}
+
 static void setup_child_process(struct child_process *cp,
 				struct add_p_state *s, ...)
 {
@@ -370,17 +382,27 @@ soft_increment:
 				if (hunk->use == UNDECIDED_HUNK)
 					hunk->use = SKIP_HUNK;
 			}
-		} else if (hunk_index && s->answer.buf[0] == 'K')
-			hunk_index--;
-		else if (hunk_index + 1 < s->hunk_nr &&
-			 s->answer.buf[0] == 'J')
-			hunk_index++;
-		else if (undecided_previous >= 0 &&
-			 s->answer.buf[0] == 'k')
-			hunk_index = undecided_previous;
-		else if (undecided_next >= 0 && s->answer.buf[0] == 'j')
-			hunk_index = undecided_next;
-		else
+		} else if (s->answer.buf[0] == 'K') {
+			if (hunk_index)
+				hunk_index--;
+			else
+				err(s, _("No previous hunk"));
+		} else if (s->answer.buf[0] == 'J') {
+			if (hunk_index + 1 < s->hunk_nr)
+				hunk_index++;
+			else
+				err(s, _("No next hunk"));
+		} else if (s->answer.buf[0] == 'k') {
+			if (undecided_previous >= 0)
+				hunk_index = undecided_previous;
+			else
+				err(s, _("No previous hunk"));
+		} else if (s->answer.buf[0] == 'j') {
+			if (undecided_next >= 0)
+				hunk_index = undecided_next;
+			else
+				err(s, _("No next hunk"));
+		} else
 			color_fprintf(stdout, s->s.help_color,
 				      _(help_patch_text));
 	}