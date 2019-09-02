matcheydj/git

built-in add -p: handle diff.algorithm
The Perl version of `git add -p` reads the config setting
`diff.algorithm` and if set, uses it to generate the diff using the
specified algorithm.

This patch ports that functionality to the C version.

To make sure that this works as intended, we add a regression test case
that tries to specify a bogus diff algorithm and then verifies that `git
diff-files` produced the expected error message.

Note: In that new test case, we actually ignore the exit code of `git
add -p`. The reason is that the C version exits with failure (as one
might expect), but the Perl version does not.

In fact, the Perl version continues happily after the uncolored diff
failed, trying to generate the colored diff, still not catching the
problem, and then it pretends to have succeeded (with exit code 0).

This is arguably a bug in the Perl version, and fixing it is safely
outside the scope of this patch.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

add-interactive.c
@@ -64,6 +64,11 @@ int init_add_i_state(struct repository *r, struct add_i_state *state)
 				  &state->interactive_diff_filter))
 		state->interactive_diff_filter = NULL;
 
+	free(state->interactive_diff_algorithm);
+	if (git_config_get_string("diff.algorithm",
+				  &state->interactive_diff_algorithm))
+		state->interactive_diff_algorithm = NULL;
+
 	return 0;
 }
 

add-patch.c
@@ -298,6 +298,7 @@ static int is_octal(const char *p, size_t len)
 static int parse_diff(struct add_p_state *state, const struct pathspec *ps)
 {
 	struct argv_array args = ARGV_ARRAY_INIT;
+	const char *diff_algorithm = state->state.interactive_diff_algorithm;
 	struct strbuf *plain = &state->plain, *colored = NULL;
 	struct child_process cp = CHILD_PROCESS_INIT;
 	char *p, *pend, *colored_p = NULL, *colored_pend = NULL, marker = '\0';
@@ -307,6 +308,8 @@ static int parse_diff(struct add_p_state *state, const struct pathspec *ps)
 	int res;
 
 	argv_array_pushv(&args, state->mode->diff);
+	if (diff_algorithm)
+		argv_array_pushf(&args, "--diff-algorithm=%s", diff_algorithm);
 	if (state->revision) {
 		struct object_id oid;
 		argv_array_push(&args,

vikkyGit/git-1

add-interactive.c
@@ -61,6 +61,11 @@ int init_add_i_state(struct repository *r, struct add_i_state *s)
 				  &s->interactive_diff_filter))
 		s->interactive_diff_filter = NULL;
 
+	free(s->interactive_diff_algorithm);
+	if (git_config_get_string("diff.algorithm",
+				  &s->interactive_diff_algorithm))
+		s->interactive_diff_algorithm = NULL;
+
 	return 0;
 }
 

add-patch.c
@@ -354,6 +354,7 @@ static int is_octal(const char *p, size_t len)
 static int parse_diff(struct add_p_state *s, const struct pathspec *ps)
 {
 	struct argv_array args = ARGV_ARRAY_INIT;
+	const char *diff_algorithm = s->s.interactive_diff_algorithm;
 	struct strbuf *plain = &s->plain, *colored = NULL;
 	struct child_process cp = CHILD_PROCESS_INIT;
 	char *p, *pend, *colored_p = NULL, *colored_pend = NULL, marker = '\0';
@@ -363,6 +364,8 @@ static int parse_diff(struct add_p_state *s, const struct pathspec *ps)
 	int res;
 
 	argv_array_pushv(&args, s->mode->diff);
+	if (diff_algorithm)
+		argv_array_pushf(&args, "--diff-algorithm=%s", diff_algorithm);
 	if (s->revision) {
 		struct object_id oid;
 		argv_array_push(&args,