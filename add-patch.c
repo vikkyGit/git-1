matcheydj/git

built-in add -p: color the prompt and the help text
... just like the Perl version ;-)

Note that this requires the `get_add_i_color()` function being defined
globally, which is the entire reason why we gave it such a descriptive
name in the first place.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

add-patch.c
@@ -331,7 +331,9 @@ static int patch_update_file(struct add_p_state *state)
 			strbuf_addstr(&state->buf, ",j");
 		if (hunk_index + 1 < state->hunk_nr)
 			strbuf_addstr(&state->buf, ",J");
-		printf(_("Stage this hunk [y,n,a,d%s,?]? "), state->buf.buf);
+		color_fprintf(stdout, state->state.prompt_color,
+			      _("Stage this hunk [y,n,a,d%s,?]? "),
+			      state->buf.buf);
 		fflush(stdout);
 		if (strbuf_getline(&state->answer, stdin) == EOF)
 			break;
@@ -373,7 +375,8 @@ soft_increment:
 		else if (undecided_next >= 0 && state->answer.buf[0] == 'j')
 			hunk_index = undecided_next;
 		else
-			puts(_(help_patch_text));
+			color_fprintf(stdout, state->state.help_color,
+				      _(help_patch_text));
 	}
 
 	/* Any hunk to be used? */

vikkyGit/git-1

add-patch.c
@@ -337,7 +337,9 @@ static int patch_update_file(struct add_p_state *s)
 			strbuf_addstr(&s->buf, ",j");
 		if (hunk_index + 1 < s->hunk_nr)
 			strbuf_addstr(&s->buf, ",J");
-		printf(_("Stage this hunk [y,n,a,d%s,?]? "), s->buf.buf);
+		color_fprintf(stdout, s->s.prompt_color,
+			      _("Stage this hunk [y,n,a,d%s,?]? "),
+			      s->buf.buf);
 		fflush(stdout);
 		if (strbuf_getline(&s->answer, stdin) == EOF)
 			break;
@@ -379,7 +381,8 @@ soft_increment:
 		else if (undecided_next >= 0 && s->answer.buf[0] == 'j')
 			hunk_index = undecided_next;
 		else
-			puts(_(help_patch_text));
+			color_fprintf(stdout, s->s.help_color,
+				      _(help_patch_text));
 	}
 
 	/* Any hunk to be used? */