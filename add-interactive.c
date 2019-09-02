matcheydj/git

built-in add -p: support interactive.diffFilter
The Perl version supports post-processing the colored diff (that is
generated in addition to the uncolored diff, intended to offer a
prettier user experience) by a command configured via that config
setting, and now the built-in version does that, too.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

add-interactive.c
@@ -59,6 +59,10 @@ int init_add_i_state(struct repository *r, struct add_i_state *state)
 	strlcpy(state->file_new_color,
 		diff_get_color(state->use_color, DIFF_FILE_NEW), COLOR_MAXLEN);
 
+	free(state->interactive_diff_filter);
+	if (git_config_get_string("interactive.difffilter",
+				  &state->interactive_diff_filter))
+		state->interactive_diff_filter = NULL;
 
 	return 0;
 }

add-patch.c
@@ -336,6 +336,7 @@ static int parse_diff(struct add_p_state *state, const struct pathspec *ps)
 
 	if (want_color_fd(1, -1)) {
 		struct child_process colored_cp = CHILD_PROCESS_INIT;
+		const char *diff_filter = state->state.interactive_diff_filter;
 
 		setup_child_process(&colored_cp, state, NULL);
 		xsnprintf((char *)args.argv[color_arg_index], 8, "--color");
@@ -345,6 +346,24 @@ static int parse_diff(struct add_p_state *state, const struct pathspec *ps)
 		argv_array_clear(&args);
 		if (res)
 			return error(_("could not parse colored diff"));
+
+		if (diff_filter) {
+			struct child_process filter_cp = CHILD_PROCESS_INIT;
+
+			setup_child_process(&filter_cp, state,
+					    diff_filter, NULL);
+			filter_cp.git_cmd = 0;
+			filter_cp.use_shell = 1;
+			strbuf_reset(&state->buf);
+			if (pipe_command(&filter_cp,
+					 colored->buf, colored->len,
+					 &state->buf, colored->len,
+					 NULL, 0) < 0)
+				return error(_("failed to run '%s'"),
+					     diff_filter);
+			strbuf_swap(colored, &state->buf);
+		}
+
 		strbuf_complete_line(colored);
 		colored_p = colored->buf;
 		colored_pend = colored_p + colored->len;
@@ -446,6 +465,9 @@ static int parse_diff(struct add_p_state *state, const struct pathspec *ps)
 						   colored_pend - colored_p);
 			if (colored_eol)
 				colored_p = colored_eol + 1;
+			else if (p != pend)
+				/* colored shorter than non-colored? */
+				goto mismatched_output;
 			else
 				colored_p = colored_pend;
 
@@ -466,6 +488,15 @@ static int parse_diff(struct add_p_state *state, const struct pathspec *ps)
 		 * to the file, so there are no trailing context lines).
 		 */
 		hunk->splittable_into++;
+
+	/* non-colored shorter than colored? */
+	if (colored_p != colored_pend) {
+mismatched_output:
+		error(_("mismatched output from interactive.diffFilter"));
+		advise(_("Your filter must maintain a one-to-one correspondence\n"
+			 "between its input and output lines."));
+		return -1;
+	}
 
 	return 0;
 }

vikkyGit/git-1

add-interactive.c
@@ -56,6 +56,10 @@ int init_add_i_state(struct repository *r, struct add_i_state *s)
 	strlcpy(s->file_new_color,
 		diff_get_color(s->use_color, DIFF_FILE_NEW), COLOR_MAXLEN);
 
+	free(s->interactive_diff_filter);
+	if (git_config_get_string("interactive.difffilter",
+				  &s->interactive_diff_filter))
+		s->interactive_diff_filter = NULL;
 
 	return 0;
 }

add-patch.c
@@ -392,6 +392,7 @@ static int parse_diff(struct add_p_state *s, const struct pathspec *ps)
 
 	if (want_color_fd(1, -1)) {
 		struct child_process colored_cp = CHILD_PROCESS_INIT;
+		const char *diff_filter = s->s.interactive_diff_filter;
 
 		setup_child_process(&colored_cp, s, NULL);
 		xsnprintf((char *)args.argv[color_arg_index], 8, "--color");
@@ -401,6 +402,24 @@ static int parse_diff(struct add_p_state *s, const struct pathspec *ps)
 		argv_array_clear(&args);
 		if (res)
 			return error(_("could not parse colored diff"));
+
+		if (diff_filter) {
+			struct child_process filter_cp = CHILD_PROCESS_INIT;
+
+			setup_child_process(&filter_cp, s,
+					    diff_filter, NULL);
+			filter_cp.git_cmd = 0;
+			filter_cp.use_shell = 1;
+			strbuf_reset(&s->buf);
+			if (pipe_command(&filter_cp,
+					 colored->buf, colored->len,
+					 &s->buf, colored->len,
+					 NULL, 0) < 0)
+				return error(_("failed to run '%s'"),
+					     diff_filter);
+			strbuf_swap(colored, &s->buf);
+		}
+
 		strbuf_complete_line(colored);
 		colored_p = colored->buf;
 		colored_pend = colored_p + colored->len;
@@ -502,6 +521,9 @@ static int parse_diff(struct add_p_state *s, const struct pathspec *ps)
 						   colored_pend - colored_p);
 			if (colored_eol)
 				colored_p = colored_eol + 1;
+			else if (p != pend)
+				/* colored shorter than non-colored? */
+				goto mismatched_output;
 			else
 				colored_p = colored_pend;
 
@@ -522,6 +544,15 @@ static int parse_diff(struct add_p_state *s, const struct pathspec *ps)
 		 * to the file, so there are no trailing context lines).
 		 */
 		hunk->splittable_into++;
+
+	/* non-colored shorter than colored? */
+	if (colored_p != colored_pend) {
+mismatched_output:
+		error(_("mismatched output from interactive.diffFilter"));
+		advise(_("Your filter must maintain a one-to-one correspondence\n"
+			 "between its input and output lines."));
+		return -1;
+	}
 
 	return 0;
 }